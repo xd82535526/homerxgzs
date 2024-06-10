87301d584b4d14bafd83763fffbf2a0c107d7e83
xd82535526 <39050465+xd82535526@users.noreply.github.com>
2024-06-10 19:26:22 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -107,7 +107,7 @@ onMounted(() => {
   const styleTitle1 = "font-size: 20px;font-weight: 600;color: rgb(244,167,89);";
   const styleTitle2 = "font-size:12px;color: rgb(244,167,89);";
   const styleContent = "color: rgb(30,152,255);";
-  const title1 = "無名の主页";
+  const title1 = "花菜՞•🐽•՞主页";
   const title2 = `
  _____ __  __  _______     ____     __
 |_   _|  \\/  |/ ____\\ \\   / /\\ \\   / /
